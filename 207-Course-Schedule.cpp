ac577de27fc2e9adc86a21d55df592b3f2e22bb8
Karan Tiwari <129376727+karantiwari28@users.noreply.github.com>
2024-08-08 15:23:30 +0530

    [207] [Time Beats: 5.92%] [Memory Beats: 100.00%] - LeetPush

---
+++ 207-Course-Schedule.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> adj[n];
+        vector<int> indegree(n, 0);
+        vector<int> ans;
+        for(auto x: pre){
+            adj[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
+        }
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            auto t = q.front();
+            ans.push_back(t);
+            q.pop();
+            for(auto x: adj[t]){
+                indegree[x]--;
+                if(indegree[x] == 0){
+                    q.push(x);
+                }
+            }
+        }
+        return ans.size() == n;
+    }
+};
